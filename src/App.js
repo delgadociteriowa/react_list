1115a12ca3a8f1a3164f411598aa36504371f314
Carlos <delgadociteriowa@gmail.com>
2024-10-28 22:45:56 -0300

    01-show-ten done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const baseUsers = [
+    { id: '1', userName: 'user1' },
+    { id: '2', userName: 'user2' },
+    { id: '3', userName: 'user3' },
+    { id: '4', userName: 'user4' },
+    { id: '5', userName: 'user5' },
+    { id: '6', userName: 'user6' },
+    { id: '7', userName: 'user7' },
+    { id: '8', userName: 'user8' },
+    { id: '9', userName: 'user9' },
+    { id: '10', userName: 'user10' },
+    { id: '11', userName: 'user11' },
+    { id: '12', userName: 'user12' },
+    { id: '13', userName: 'user13' },
+    { id: '14', userName: 'user14' },
+    { id: '15', userName: 'user15' },
+    { id: '16', userName: 'user16' },
+    { id: '17', userName: 'user17' },
+    { id: '18', userName: 'user18' },
+    { id: '19', userName: 'user19' },
+    { id: '20', userName: 'user20' },
+  ];
+
+  const [users, setUsers] = useState(baseUsers);
+  const [limit, setLimit] = useState(10);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.slice(0, limit).map((user) => (
+          <li key={user.id}>{user.userName}</li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.js';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Users App', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('Shows Users Title', () => {
+    const headingElement = screen.getByRole('heading', { name: /Users/i });
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  it('Shows five users on initial render', () => {
+    const listItems = screen.getAllByRole('listitem').length;
+    expect(listItems).toBe(10);
+  });
 });
