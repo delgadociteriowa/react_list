270998f7c123950fcecc57320c0d4a4bd05aa631
Carlos <delgadociteriowa@gmail.com>
2024-11-17 21:47:40 -0300

    07-use-context done

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import Users from './components/Users';
+import UsersState from './context/UsersState';
+import UsersContext from './context/UsersContext';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [usersCount, setUsersCount] = useState(10);
-  const url = 'https://api.github.com/users?per_page=20';
-
-  const showMore = () => {
-    setUsersCount(usersCount + 5);
-  };
-
-  const showLess = () => {
-    setUsersCount(usersCount - 5);
-  };
-
-  useEffect(() => {
-    const getUsers = async () => {
-      try {
-        const data = await fetch(url);
-        console.log(data);
-        if (!data.ok) {
-          console.log('Data Error');
-        }
-        const fetchUsers = await data.json();
-        setUsers(fetchUsers);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getUsers();
-  }, []);
-
+  const usersContext = useContext(UsersContext);
+  const { usersCount, showMore, showLess } = usersContext;
   return (
     <>
-      <h1>Users</h1>
-      <Users users={users} usersCount={usersCount} />
-      {usersCount === 20 ? null : <button onClick={showMore}>More</button>}
-      {usersCount === 0 ? null : <button onClick={showLess}>Less</button>}
+      <UsersState>
+        <h1>Users</h1>
+        <Users />
+        {usersCount === 20 ? null : <button onClick={showMore}>More</button>}
+        {usersCount === 0 ? null : <button onClick={showLess}>Less</button>}
+      </UsersState>
     </>
   );
 };
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import UsersContext from '../context/UsersContext';
 
-const Users = ({ users, usersCount }) => {
+const Users = () => {
+  const usersContext = useContext(UsersContext);
+  const { users, usersCount } = usersContext;
   return (
     <ul>
       {users.slice(0, usersCount).map((user) => (
