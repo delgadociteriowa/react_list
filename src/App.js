a5152345a8e23a852d81e96e41627d1c7d56fd5e
Carlos <delgadociteriowa@gmail.com>
2024-11-18 20:16:42 -0300

    09-use-ref done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Users from './components/Users';
 import UsersControls from './components/UsersControls';
 import UserState from './context/user/UserState';
 
 const App = () => {
+  const timerRef = useRef(null);
+  const inactivityTime = 20000;
+
+  const resetTimer = () => {
+    // Cleans previous timer
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    // Starts new timer
+    timerRef.current = setTimeout(() => {
+      alert('You have been inactive for 20 seconds!');
+    }, inactivityTime);
+  };
+
+  useEffect(() => {
+    // Resets timer on any interaction
+    const handleUserActivity = () => resetTimer();
+
+    // Interaction events
+    window.addEventListener('mousemove', handleUserActivity);
+    window.addEventListener('keydown', handleUserActivity);
+
+    // Starts timer for the first time
+    resetTimer();
+
+    // Cleans events and timer on unmount
+    return () => {
+      clearTimeout(timerRef.current);
+      window.removeEventListener('mousemove', handleUserActivity);
+      window.removeEventListener('keydown', handleUserActivity);
+    };
+  }, []);
+
   return (
     <>
       <UserState>
--- src/components/UsersControls.js
+++ src/components/UsersControls.js
@@ -1,12 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import UserContext from '../context/user/userContext';
 
 const UsersControls = () => {
   const userContext = useContext(UserContext);
   const { usersCount, showMore, showLess } = userContext;
+
+  const buttonRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    buttonRef.current.style.fontWeight = 'bold';
+  };
+
+  const handleMouseLeave = () => {
+    buttonRef.current.style.fontWeight = 'normal';
+  };
   return (
     <>
-      {usersCount === 20 ? null : <button onClick={showMore}>More</button>}
+      {usersCount === 20 ? null : (
+        <button
+          ref={buttonRef}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onClick={showMore}
+        >
+          More
+        </button>
+      )}
       {usersCount === 0 ? null : <button onClick={showLess}>Less</button>}
     </>
   );
